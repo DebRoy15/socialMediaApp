79b6d33365480cc84f199d87d35ffc22c9836967
DebRoy15 <debjyoti.roy1652@gmail.com>
2022-05-22 16:45:01 +0530

    redesign some webpages

--- client-app/src/app/store/activityStore.ts
+++ client-app/src/app/store/activityStore.ts
@@ -19,6 +19,17 @@ export default class ActivityStore {
       (a, b) => Date.parse(a.date) - Date.parse(b.date)
     );
   }
+  get groupActivities() {
+    return Object.entries(
+      this.activitiesByDate.reduce((activities, activity) => {
+        const date = activity.date;
+        activities[date] = activities[date]
+          ? [...activities[date], activity]
+          : [activity];
+        return activities;
+      }, {} as { [key: string]: Activity[] })
+    );
+  }
 
   loadActivities = async () => {
     this.loadingInitial = true;
@@ -43,9 +54,9 @@ export default class ActivityStore {
       try {
         activity = await agent.Activities.details(id);
         this.setActivity(activity);
-        runInAction(() =>{
+        runInAction(() => {
           this.selectedActivity = activity;
-        })
+        });
         this.setLoadingInitial(false);
         return activity;
       } catch (error) {
