985e5f59b43c3f42db96635e53ba2e472f42aa75
DebRoy15 <debjyoti.roy1652@gmail.com>
2022-06-07 00:01:18 +0530

    image upload implementation in API

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -21,45 +21,50 @@ namespace API.Controllers
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
         private readonly TokenService _tokenService;
-        public AccountController(UserManager<AppUser> userManager, 
-            SignInManager<AppUser> signInManager, TokenService tokenService){
-                _tokenService = tokenService;
-                _userManager = userManager;
-                _signInManager = signInManager;
+        public AccountController(UserManager<AppUser> userManager,
+            SignInManager<AppUser> signInManager, TokenService tokenService)
+        {
+            _tokenService = tokenService;
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
-        
+
         [Authorize]
         [HttpGet]
-        public async Task<ActionResult<UserDto>> GetCurrentUser(){
-            var user = await _userManager.FindByEmailAsync(User.FindFirstValue(ClaimTypes.Email));
-            
+        public async Task<ActionResult<UserDto>> GetCurrentUser()
+        {
+            var user = await _userManager.Users.Include(p => p.Photos)
+            .FirstOrDefaultAsync(x => x.Email == User.FindFirstValue(ClaimTypes.Email));
+
             return CreateUserObject(user);
         }
 
         [HttpPost("login")]
-        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto ){
-           var user = await _userManager.FindByEmailAsync(loginDto.Email);
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _userManager.Users.Include(p => p.Photos)
+            .FirstOrDefaultAsync(x => x.Email == loginDto.Email);
 
-           if(user == null) return Unauthorized();
+            if (user == null) return Unauthorized();
 
-           var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password,false);
-          
-           if(result.Succeeded)
-           return CreateUserObject(user);
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
 
-           return Unauthorized();
+            if (result.Succeeded)
+                return CreateUserObject(user);
+
+            return Unauthorized();
         }
 
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
-            if(await _userManager.Users.AnyAsync( x => x.Email == registerDto.Email))
+            if (await _userManager.Users.AnyAsync(x => x.Email == registerDto.Email))
             {
                 ModelState.AddModelError("email", "Email taken");
                 return ValidationProblem();
             }
 
-            if(await _userManager.Users.AnyAsync( x => x.UserName == registerDto.UserName))
+            if (await _userManager.Users.AnyAsync(x => x.UserName == registerDto.UserName))
             {
                 ModelState.AddModelError("username", "Username taken");
                 return ValidationProblem();
@@ -72,10 +77,10 @@ namespace API.Controllers
                 UserName = registerDto.UserName
             };
 
-            var result =await _userManager.CreateAsync(user, registerDto.Password);
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
 
-            if(result.Succeeded)
-            return CreateUserObject(user);
+            if (result.Succeeded)
+                return CreateUserObject(user);
 
             return BadRequest("Problem registering user");
         }
@@ -85,7 +90,7 @@ namespace API.Controllers
             return new UserDto
             {
                 DisplayName = user.DisplayName,
-                Image = null,
+                Image = user?.Photos?.FirstOrDefault(x => x.IsMain)?.Url,
                 Token = _tokenService.CreateToken(user),
                 Username = user.UserName
             };
