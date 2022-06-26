43d4cbfb5edd9c40a6a640216f602844eb906056
DebRoy15 <debjyoti.roy1652@gmail.com>
2022-06-26 23:55:00 +0530

    implemented followers and following logic

--- client-app/src/features/activities/details/ActivityDetailSidebar.tsx
+++ client-app/src/features/activities/details/ActivityDetailSidebar.tsx
@@ -9,7 +9,7 @@ interface Props {
 }
 
 export default observer(function ActivityDetailedSidebar({
-  activity: { attendees, host , isHost},
+  activity: { attendees, host, isHost },
 }: Props) {
   if (!attendees) return null;
   return (
@@ -28,7 +28,7 @@ export default observer(function ActivityDetailedSidebar({
         <List relaxed divided>
           {attendees.map((attendee) => (
             <Item style={{ position: "relative" }} key={attendee.username}>
-              { attendee.username === host?.username && (
+              {attendee.username === host?.username && (
                 <Label
                   style={{ position: "absolute" }}
                   color="orange"
@@ -36,7 +36,7 @@ export default observer(function ActivityDetailedSidebar({
                 >
                   Host
                 </Label>
-              ) }
+              )}
               <Image size="tiny" src={attendee.image || "/assets/user.png"} />
               <Item.Content verticalAlign="middle">
                 <Item.Header as="h3">
@@ -44,7 +44,9 @@ export default observer(function ActivityDetailedSidebar({
                     {attendee.displayName}
                   </Link>
                 </Item.Header>
-                <Item.Extra style={{ color: "orange" }}>Following</Item.Extra>
+                {attendee.following && (
+                  <Item.Extra style={{ color: "orange" }}>Following</Item.Extra>
+                )}
               </Item.Content>
             </Item>
           ))}
